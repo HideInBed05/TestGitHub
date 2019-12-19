18c4893cbc66c89970b8f8c6986f3f8f3bf0d56c
Administrator <1318734528@qq.com>
2019-12-19 10:33:57 +0800

    第一次嘻嘻嘻

--- src/main/java/Test01.java
+++ src/main/java/Test01.java
@@ -1,6 +1,6 @@
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("xxxx");
-        System.out.println("xixixixix");
+        System.out.println("xixixixixxi");
     }
 }
